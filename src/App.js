a81f4edc835f3bd594457520e98731b1c055f0b1
conniepocky <c.amer@icloud.com>
2018-10-02 20:29:00 +0100

    yay

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <iframe src="https://giphy.com/tv/search/cats" width="1200px" height="1200px"></iframe>
+      //<audio src="/assets/sims.mp3" controls></audio>
+    );
+  }
+}
+
+export default App;
