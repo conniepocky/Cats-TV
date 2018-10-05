0d4dc013b2719e2b5bdbcaf14fb93c144e89e136
conniepocky <c.amer@icloud.com>
2018-10-05 19:57:05 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <iframe src="https://giphy.com/tv/search/cats" width="1200px" height="1200px"></iframe>
-      //<audio src="/assets/sims.mp3" controls></audio>
+      <div>
+        <iframe src="https://giphy.com/tv/search/cats" width="1200px" height="1200px" title="cats!!!!!!!!!!!!!!!!"></iframe>
+        <audio src="/assets/sims.mp3" controls></audio>
+      </div>
+      
     );
   }
 }
